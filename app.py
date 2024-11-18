70552473dfdfcb5ca5f90492dc7a1e72150d7dbd
nahodkins <tsuber2001@gmail.com>
2024-11-19 01:19:45 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({"status": "OK"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
