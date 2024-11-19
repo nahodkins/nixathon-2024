bbb54a6aff9589b7a1d65d02858acffda98eee4d
nahodkins <tsuber2001@gmail.com>
2024-11-19 20:10:17 +0200

    Renames endpoint

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/status', methods=['GET'])
+@app.route('/healthz', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
 
